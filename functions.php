1e2fbe2a32b62f37d891d407587ccf91114b89a6
Riad Benguella <benguella@gmail.com>
2021-12-28 13:19:43 +0100

    Add a string translation

--- functions.php
+++ functions.php
@@ -5,3 +5,8 @@ function kubrik2_theme_support()  {
 	add_theme_support( 'wp-block-styles' );
 }
 add_action( 'after_setup_theme', 'kubrik2_theme_support' );
+
+// This is useless, but it's just a way to avoid a bug
+// in the theme repository submission process
+// where at least one i18n string is required.
+__( 'Hello, World!', 'kubrick2' );
